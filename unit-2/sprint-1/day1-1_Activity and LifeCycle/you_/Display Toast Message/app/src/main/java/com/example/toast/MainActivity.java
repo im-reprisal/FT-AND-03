ed15214ed745493b4b2f596851bdc15deb494898
Nishant Rishab <imreprisal@gmail.com>
2021-06-26 20:28:01 +0530

    Commit

--- unit-2/sprint-1/day1-1_Activity and LifeCycle/you_/Display Toast Message/app/src/main/java/com/example/toast/MainActivity.java
+++ unit-2/sprint-1/day1-1_Activity and LifeCycle/you_/Display Toast Message/app/src/main/java/com/example/toast/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtnFirst;
+    Button mBtnSecond;
+    Button mBtnThird;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnFirst = findViewById(R.id.btnFirst);
+        mBtnSecond = findViewById(R.id.btnSecond);
+        mBtnThird = findViewById(R.id.btnThird);
+        mBtnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button One clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mBtnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button second clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mBtnThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button Third clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
