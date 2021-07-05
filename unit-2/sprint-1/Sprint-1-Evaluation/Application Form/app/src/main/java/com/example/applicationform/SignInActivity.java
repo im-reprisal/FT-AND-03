843a3c0b8a194c1d00655f11d969f2747657775c
im-reprisal <imreprisal@gmail.com>
2021-07-05 13:44:14 +0530

    Commit

--- unit-2/sprint-1/Sprint-1-Evaluation/Application Form/app/src/main/java/com/example/applicationform/SignInActivity.java
+++ unit-2/sprint-1/Sprint-1-Evaluation/Application Form/app/src/main/java/com/example/applicationform/SignInActivity.java
@@ -28,5 +28,33 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        mBtnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUserNameValid = isUserNameValid();
+                boolean isPasswordValid = isPasswordValid();
+                if (isPasswordValid && isUserNameValid) {
+                    Intent intent = new Intent(SignInActivity.this, HomeActivity.class);
+                    intent.putExtra("username",mEtEnterName.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    private boolean isPasswordValid() {
+        if (mEtEnterPassword.getText().toString().length() >= 6) {
+            return true;
+        } else {
+            mEtEnterPassword.setError("Password is weak.");
+            return false;
+        }
+    }
+    private boolean isUserNameValid() {
+        if (mEtEnterName.getText().toString().trim().length() >= 4) {
+            return true;
+        } else {
+            mEtEnterName.setError("Minimum 4 characters needed");
+            return false;
+        }
     }
 }
