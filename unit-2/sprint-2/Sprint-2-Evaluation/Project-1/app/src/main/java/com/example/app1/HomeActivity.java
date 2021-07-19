991fd42b5e713bd9decf9a36c3476ff3e707f8d9
Nishant <imreprisal@gmail.com>
2021-07-19 15:50:47 +0530

    commit

--- unit-2/sprint-2/Sprint-2-Evaluation/Project-1/app/src/main/java/com/example/app1/HomeActivity.java
+++ unit-2/sprint-2/Sprint-2-Evaluation/Project-1/app/src/main/java/com/example/app1/HomeActivity.java
@@ -1,14 +1,51 @@
 package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<ResponseModel> responseModel = new ArrayList<>();
+    private ResponseAdapter responseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        recyclerView = findViewById(R.id.recyclerView);
+        getList();
+        setRecyclerData();
+    }
+
+    private void setRecyclerData() {
+        responseAdapter = new ResponseAdapter((ArrayList<ResponseModel>) responseModel);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setAdapter(responseAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private void getList() {
+        ApiService apiservice = Network.getInstance().create(ApiService.class);
+        Call<List<ResponseModel>> call = apiservice.getUser();
+        call.enqueue(new Callback<List<ResponseModel>>() {
+            @Override
+            public void onResponse(Call<List<ResponseModel>> call, Response<List<ResponseModel>> response) {
+                responseModel = response.body();
+                responseAdapter.updateData(responseModel);
+
+            }
+
+            @Override
+            public void onFailure(Call<List<ResponseModel>> call, Throwable t) {
+
+            }
+        });
     }
 }
