95cd251c04312f396d5020cd615836b54ad2a1d2
Nishant <imreprisal@gmail.com>
2021-07-20 00:30:31 +0530

    commit

--- unit-2/sprint-1/day-9-Constraint Layout/assignments/Open Masai School/app/src/main/java/com/example/myapplication/MainActivity.java
+++ unit-2/sprint-1/day-9-Constraint Layout/assignments/Open Masai School/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mBtnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnClick = findViewById(R.id.btnClick);
+        mBtnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mintent = new Intent();
+                mintent.setAction(Intent.ACTION_VIEW);
+                mintent.setData(Uri.parse("https://www.masaischool.com/"));
+                Toast.makeText(MainActivity.this, "Found", Toast.LENGTH_SHORT).show();
+
+                if (mintent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mintent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Not Found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
