0291c1f57d54467bb9de202df2b6ae3f8c54d4be
Nishant Rishab <imreprisal@gmail.com>
2021-08-06 01:29:19 +0530

    commit

--- unit-3/sprint-1/day-7_Threads/we/app/src/main/java/com/example/myapplication/WorkerThread.java
+++ unit-3/sprint-1/day-7_Threads/we/app/src/main/java/com/example/myapplication/WorkerThread.java
@@ -2,20 +2,13 @@ package com.example.myapplication;
 
 import android.os.Handler;
 import android.os.Looper;
-
 public class WorkerThread extends Thread {
-
-
     private Handler handler;
-
-
     @Override
     public void run() {
         super.run();
         Looper.prepare();
-
         handler = new Handler(Looper.myLooper());
-
         Looper.loop();
     }
     public void addTaskToMessageQueue(Runnable task){
