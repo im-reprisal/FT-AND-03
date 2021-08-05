808996757fd02259ef14ce142af5002a92c40743
Nishant Rishab <imreprisal@gmail.com>
2021-08-06 00:40:16 +0530

    commit

--- unit-3/sprint-1/day-6_View Pager/assignments/ViewPager with circle indicator/app/src/main/java/com/example/imageslider/MainActivity.java
+++ unit-3/sprint-1/day-6_View Pager/assignments/ViewPager with circle indicator/app/src/main/java/com/example/imageslider/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.imageslider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    ArrayList<Integer> imagesList = new ArrayList<>();
+    ViewPagerAdapter viewPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.viewPager);
+        imagesList.add(R.drawable.img1);
+        imagesList.add(R.drawable.img2);
+        imagesList.add(R.drawable.img3);
+        imagesList.add(R.drawable.img4);
+        viewPagerAdapter = new ViewPagerAdapter(this,imagesList);
+        viewPager.setPadding(100,0,100,0);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
---
+++ unit-3/sprint-1/day-6_View Pager/assignments/ViewPager with circle indicator/app/src/main/java/com/example/imageslider/ViewPagerAdapter.java
@@ -0,0 +1,55 @@
+package com.example.imageslider;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    ImageView mIvImageView;
+    Context context;
+    ArrayList<Integer> imagesList;
+
+    public ViewPagerAdapter(Context context, ArrayList<Integer> imagesList) {
+        this.context = context;
+        this.imagesList = imagesList;
+    }
+
+    @Override
+    public int getCount() {
+        return imagesList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.indicator_itemlayout,null);
+        mIvImageView = view.findViewById(R.id.ivImageView);
+        Glide.with(context)
+        .asBitmap()
+        .load(imagesList.get(position))
+        .into(mIvImageView);
+
+        container.addView(view,0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.destroyItem(container, position, object);
+
+    }
+}
