a0802c27f16c4cde96c73ecf8890bd74d8b18db1
Nishant Rishab <imreprisal@gmail.com>
2021-09-14 00:17:16 +0530

    Commit

---
+++ unit-4/sprint-2/Live Data/you/Android LiveData assignment between Fragments/app/src/main/java/com/example/livedata/FragmentA.kt
@@ -0,0 +1,22 @@
+package com.example.livedata
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_a.*
+
+
+class FragmentA : Fragment(R.layout.fragment_a) {
+    private val dataClass = DataClass()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnIncCountFragA.setOnClickListener {
+            dataClass.incFragACount()
+        }
+        dataClass.getMediatorLiveData().observe(viewLifecycleOwner, Observer {
+            tvShowCountA.text = it
+        })
+    }
+}
