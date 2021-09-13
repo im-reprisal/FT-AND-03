376abf2868e0504827e5b28f0b985fb6f2966082
Nishant Rishab <imreprisal@gmail.com>
2021-09-13 15:03:24 +0530

    second

--- unit-4/sprint-2/Evaluation/app/src/main/java/com/example/myapplication/DatabaseHandler.kt
+++ unit-4/sprint-2/Evaluation/app/src/main/java/com/example/myapplication/DatabaseHandler.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -10,7 +11,7 @@ class DatabaseHandler(private val context:Context):SQLiteOpenHelper(context,DB_N
         val DB_VERSION = 1
         val TABLE_NAME = "store"
         val ID = "id"
-        val ITEM_NAME = "itemName"
+        val ITEM_NAME = "itemname"
         val PRICE = "price"
         val DESC = "desc"
     }
@@ -23,6 +24,38 @@ class DatabaseHandler(private val context:Context):SQLiteOpenHelper(context,DB_N
                 "$PRICE INTEGER, " +
                 "$DESC TEXT)"
         db?.execSQL(CREATE_TABLE_QUERY)
+    }
+    fun insertTask(itemname:String,price:Int,desc:String){
+        val db = writableDatabase
+        val values = ContentValues()
+        values.put(ITEM_NAME,itemname)
+        values.put(PRICE,price)
+        values.put(DESC,desc)
+        val idValue = db.insert(TABLE_NAME,null,values)
+    }
+    fun getAllTask() :MutableList<Task>{
+        val taskList = mutableListOf<Task>()
+        val db = readableDatabase
+        val query = "select * from $TABLE_NAME"
+        val cursor = db.rawQuery(query,null)
+        if (cursor != null && cursor.count > 0){
+            cursor.moveToFirst()
+        }
+        do {
+            val id = cursor.getInt(cursor.getColumnIndex(ID))
+            val price = cursor.getInt(cursor.getColumnIndex(PRICE))
+            val itemname = cursor.getString(cursor.getColumnIndex(ITEM_NAME))
+            val desc = cursor.getString(cursor.getColumnIndex(DESC))
+
+            val task = Task()
+            task.id = id
+            task.price = price
+            task.itemname = itemname
+            task.desc= desc
+
+            taskList.add(task)
+        } while (cursor.moveToNext())
+        return taskList
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
---
+++ unit-4/sprint-2/Evaluation/app/src/main/java/com/example/myapplication/Task.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+class Task {
+    var id = 0
+    var price = 0
+    var itemname = ""
+    var desc = ""
+}
