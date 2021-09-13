50ce2ef5aeaa1210a5cfb200cf9392746b842d69
Nishant Rishab <imreprisal@gmail.com>
2021-09-13 17:14:58 +0530

    fifth

--- unit-4/sprint-2/Evaluation/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ unit-4/sprint-2/Evaluation/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var sqliteHelper : DatabaseHandler
     private lateinit var recyclerView : RecyclerView
-    private var adapter : ItemAdapter? =null
+    private lateinit var adapter : ItemAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun getData() {
        val taskList = sqliteHelper.getAllItem()
+        recyclerView.adapter=adapter
         adapter?.addItems(taskList as ArrayList<Task>)
     }
 
@@ -35,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         val desc = etDesc.text.toString()
         val itm = Task(itemname = itemname , price = price , desc = desc)
         sqliteHelper.insertItem(itm)
+        Toast.makeText(this,"Saved",Toast.LENGTH_SHORT).show()
     }
     fun initRecyclerView(){
         recyclerView.layoutManager = LinearLayoutManager(this)
