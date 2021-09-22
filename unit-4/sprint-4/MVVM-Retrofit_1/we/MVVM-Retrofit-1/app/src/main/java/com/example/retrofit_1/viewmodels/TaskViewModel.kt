e2a0415bf52e2e3e4ae195f8c766356eda7d5154
Kotlin <imreprisal@gmail.com>
2021-09-22 10:07:46 +0530

    MVVM using API

--- unit-4/sprint-4/MVVM-Retrofit_1/we/MVVM-Retrofit-1/app/src/main/java/com/example/retrofit_1/viewmodels/TaskViewModel.kt
+++ unit-4/sprint-4/MVVM-Retrofit_1/we/MVVM-Retrofit-1/app/src/main/java/com/example/retrofit_1/viewmodels/TaskViewModel.kt
@@ -3,7 +3,6 @@ package com.example.retrofit_1.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
-
 import com.example.retrofit_1.models.remote.Resource
 import com.example.retrofit_1.models.remote.requests.LoginRequestModel
 import com.example.retrofit_1.models.remote.responses.LoginResponse
