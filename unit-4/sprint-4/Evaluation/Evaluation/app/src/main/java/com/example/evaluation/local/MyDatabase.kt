8a4af65649f3d6ec50a3b244ddb99ce530fbc64c
Kotlin <imreprisal@gmail.com>
2021-09-27 15:20:50 +0530

    fourth

--- unit-4/sprint-4/Evaluation/Evaluation/app/src/main/java/com/example/evaluation/local/MyDatabase.kt
+++ unit-4/sprint-4/Evaluation/Evaluation/app/src/main/java/com/example/evaluation/local/MyDatabase.kt
@@ -1,11 +1,28 @@
 package com.example.evaluation.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 
 @Database(entities = [MyEntity::class],version = 1)
 abstract class MyDatabase {
     abstract fun getMyDao(): MyDao
     companion object{
-        private 
+        private var INSTANCE:MyDatabase? = null
+        fun getDatabase(context: Context):MyDatabase{
+            if (INSTANCE == null){
+                val builder = Room.databaseBuilder(
+                    context.applicationContext,
+                    MyDatabase::class.java,
+                    "my_database"
+                )
+                    builder.fallbackToDestructiveMigration()
+                    INSTANCE = build()
+                return INSTANCE!!
+            }
+            else{
+                return INSTANCE!!
+            }
+        }
     }
 }
