cb4097742f01ebc1ec3a482e56044bb16589b351
Kotlin <imreprisal@gmail.com>
2021-09-27 16:08:38 +0530

    sixth

--- unit-4/sprint-4/Evaluation/Evaluation/app/src/main/java/com/example/evaluation/local/MyEntity.kt
+++ unit-4/sprint-4/Evaluation/Evaluation/app/src/main/java/com/example/evaluation/local/MyEntity.kt
@@ -2,8 +2,6 @@ package com.example.evaluation.local
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 @Entity(tableName = "my_cart_table")
 data class MyEntity(
     @ColumnInfo(name = "name") val name: String,
