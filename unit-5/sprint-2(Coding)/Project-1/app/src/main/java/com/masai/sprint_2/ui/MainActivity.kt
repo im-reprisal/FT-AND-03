f89c792906f3b573ca927f526686406def670201
Nishant Rishab <imreprisal@gmail.com>
2021-10-18 17:29:34 +0530

    eight

--- unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/ui/MainActivity.kt
+++ unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/ui/MainActivity.kt
@@ -2,11 +2,42 @@ package com.masai.sprint_2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.masai.sprint_2.R
+import com.masai.sprint_2.ui.adapter.MainAdapter
+import com.masai.sprint_2.ui.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewModel:MainViewModel
+    private lateinit var mainAdapter: MainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        setAdapter()
+        mainViewModel.searchPeopleList().observe(
+            this,{
+                it?.let {
+                    CoroutineScope(Main).launch {
+                        mainAdapter.submitData(it)
+                    }
+                }
+            }
+        )
+    }
+
+    private fun setAdapter() {
+        mainAdapter = MainAdapter()
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.apply {
+            layoutManager = linearLayoutManager
+            this.adapter = mainAdapter
+        }
     }
 }
--- unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/ui/viewmodel/MainViewModel.kt
+++ unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/ui/viewmodel/MainViewModel.kt
@@ -1,7 +1,11 @@
 package com.masai.sprint_2.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.masai.sprint_2.ui.PeopleRepo
 
 class MainViewModel:ViewModel() {
 
+    private val peopleRepo = PeopleRepo()
+
+    fun searchPeopleList() = peopleRepo.getPagesList()
 }
