bf3dc36feef517a9d8324e85e0ba0151402c7c14
Nishant Rishab <imreprisal@gmail.com>
2021-10-18 15:01:08 +0530

    third

--- unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/data/local/MyDao.kt
+++ unit-5/sprint-2(Coding)/Project-1/app/src/main/java/com/masai/sprint_2/data/local/MyDao.kt
@@ -1,5 +1,16 @@
 package com.masai.sprint_2.data.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
+@Dao
 interface MyDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertItem(myEntity: MyEntity)
+
+    @Query("select * from person_data_table")
+    fun getItems(): LiveData<List<MyEntity>>
 }
