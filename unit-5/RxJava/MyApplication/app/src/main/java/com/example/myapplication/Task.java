ca8015acf2f77667f88a4853a92a43b085de64b0
Nishant Rishab <imreprisal@gmail.com>
2021-10-29 15:58:34 +0530

    c

---
+++ unit-5/RxJava/MyApplication/app/src/main/java/com/example/myapplication/Task.java
@@ -0,0 +1,73 @@
+package com.example.myapplication;
+
+public class Task {
+    private int id;
+    private String name;
+    private Double price;
+
+    public Task(int id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
+
+class StudentDetails {
+    private String studentClass;
+    private int studentId;
+    private String studentName;
+
+    public StudentDetails(String studentClass, int studentId, String studentName) {
+        this.studentClass = studentClass;
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentClass() {
+        return studentClass;
+    }
+
+    public void setStudentClass(String studentClass) {
+        this.studentClass = studentClass;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
