bdc9487c8a463ffb86446f49b60bffda68cac8da
Nishant Rishab <imreprisal@gmail.com>
2021-11-08 14:37:09 +0530

    second

---
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/data/local/MyDatabase.kt
@@ -0,0 +1,4 @@
+package com.example.my.data.local
+
+abstract class MyDatabase {
+}
--- unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/ui/MainActivity.kt
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.my
+package com.example.my.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.my.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
