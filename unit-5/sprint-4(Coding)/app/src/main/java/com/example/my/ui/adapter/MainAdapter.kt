a6b43310e19965557fffaf572fb87b7a4d9e0a51
Nishant Rishab <imreprisal@gmail.com>
2021-11-08 15:15:32 +0530

    fourth

---
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/ui/adapter/MainAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.my.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.my.R
+import com.example.my.data.models.Article
+import com.example.my.ui.viewholder.MainViewHolder
+
+class MainAdapter(private val articleList : List<Article>): RecyclerView.Adapter<MainViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false)
+        return MainViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val resultList = articleList[position]
+        holder.setData(resultList)
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+}
---
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/ui/viewholder/MainViewHolder.kt
@@ -0,0 +1,18 @@
+package com.example.my.ui.viewholder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.my.data.models.Article
+import kotlinx.android.synthetic.main.item_layout.view.*
+
+class MainViewHolder(private val view: View):RecyclerView.ViewHolder(view) {
+    fun setData(article: Article){
+        view.apply {
+            Glide.with(this).load(article.urlToImage).into(iv_article_image)
+            tv_Title.text = article.title
+            tv_Date.text = article.publishedAt
+            tv_Description.text = article.description
+        }
+    }
+}
