d1ef8f2abb2c86d3b1f22b2e0d79eccf2d3af576
Nishant Rishab <imreprisal@gmail.com>
2021-11-08 14:55:58 +0530

    third

--- unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/network/ApiService.kt
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/network/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.my.network
 
+import com.example.my.data.models.ResponseModel
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("v2/everything?q=tesla&from=2021-10-08&sortBy=publishedAt&apiKey=24ee3463f67844e2b4d131595a0575ec")
+    suspend fun getAPIResponse():ResponseModel
 }
--- unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/network/ServiceGenerator.kt
+++ unit-5/sprint-4(Coding)/app/src/main/java/com/example/my/network/ServiceGenerator.kt
@@ -1,4 +1,13 @@
 package com.example.my.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ServiceGenerator {
+    private fun getRetrofit() = Retrofit.Builder()
+        .baseUrl("https://newsapi.org/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getApiService() = getRetrofit().create(ApiService::class.java)
 }
